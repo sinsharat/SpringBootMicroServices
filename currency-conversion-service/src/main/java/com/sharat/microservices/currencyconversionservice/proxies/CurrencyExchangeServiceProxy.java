c55f603bf310de330c88fd8720b97941d2867a6e
sinsharat@gmail.com <sinsharat@gmail.com>
2020-01-27 20:07:17 +0530

    Added API gateway and configured it with microservices

--- currency-conversion-service/src/main/java/com/sharat/microservices/currencyconversionservice/proxies/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/sharat/microservices/currencyconversionservice/proxies/CurrencyExchangeServiceProxy.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sharat.microservices.currencyconversionservice.bean.CurrencyConversion;
 
-//@FeignClient(name="currency-exchange-service", url="localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000") // connects to service using url
+// @FeignClient(name="currency-exchange-service") // connects to the service using eureka
+@FeignClient(name="netflix-zuul-api-gateway-server") // connects to the service using eureka via API gateway
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	// @GetMapping("/currency-exchange/from/{from}/to/{to}") used without api gateway to directly hit the exact url
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}") // when used with api gateway. the invocation api application name is appended
 	public CurrencyConversion retrieveCurrencyExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 	
 }
